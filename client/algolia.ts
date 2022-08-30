6b6e0fe7eeebf2e3b46a84cbd093778deaea5112
CreateWebSiteTeam <yourgitemail@example.com>
2022-08-30 23:19:37 +0900

    env

--- client/algolia.ts
+++ client/algolia.ts
@@ -1,8 +1,11 @@
 import algoliasearch from "algoliasearch";
-
+const apikey = process.env.ALGOLIA_APPLICATION_ID
+const adminkey =  process.env.ALGOLIA_ADMIN_API_KEY 
 const client = algoliasearch(
-  "K4YWALFPTU",
-  "a3ad4f844add0e5ac1e02ff73e9da09e"
-);
+  // "K4YWALFPTU",
+  // "a3ad4f844add0e5ac1e02ff73e9da09e"
+  apikey as string,
+  adminkey as string
+  );
 
 export default client;
